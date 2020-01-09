5205b9b7a71e8fd3dad8cc01ce79fffcb2ab6aa8
weijieGuo <guo.weijie@outlook.com>
2020-01-09 09:28:55 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+
+  },
+  data(){
+
+    return {
+      a:{}
+    }
+  }
+
+}
+</script>
+
+<style>
+@import './assets/scss/reset.scss'
+</style>
